9976e44fbd9994703b6312e5be999de5a475ed2f
code-today <blogarpit21@gmail.com>
2023-02-09 23:45:35 +0530

    update unavailable poster placholder images

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,15 @@
 //image sizes for tmdb
 export const img_300 = "https://image.tmdb.org/t/p/w300";
 export const img_500 = "https://image.tmdb.org/t/p/w500";
+
+// content model and single content
+export const unavailable =
+  "https://critics.io/img/movies/poster-placeholder.png";
+
+// contentModal
+export const unavailableLandscape =
+  "https://user-images.githubusercontent.com/10515204/56117400-9a911800-5f85-11e9-878b-3f998609a6c8.jpg";
+
+// For Carousel
+export const noPicture =
+  "https://upload.wikimedia.org/wikipedia/en/6/60/No_Picture.jpg";
