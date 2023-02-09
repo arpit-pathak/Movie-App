7496c509e57bdf371e69d5dcd937d7ed80d1754d
code-today <blogarpit21@gmail.com>
2023-02-09 23:54:57 +0530

    fetch data from api with axios

--- src/Pages/Trending/Trending.js
+++ src/Pages/Trending/Trending.js
@@ -1,9 +1,24 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 const Trending = () => {
+  const [content, setcontent] = useState([]);
+
+  const fetchTrending = async () => {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}`
+    );
+    // console.log(data.results);
+    setcontent(data.results);
+  };
+
+  useEffect(() => {
+    fetchTrending();
+  }, []);
+
   return (
     <div>
       <span className="pageTitle">Trending page</span>
+      <div className="trending"></div>
     </div>
   );
 };
