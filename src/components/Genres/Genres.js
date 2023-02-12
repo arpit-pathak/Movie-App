9d43f00a9e7ebcdf6f5502f1a969fab7c32d6ff2
code-today <blogarpit21@gmail.com>
2023-02-12 23:53:41 +0530

    created Genres component with custom hook

---
+++ src/components/Genres/Genres.js
@@ -0,0 +1,77 @@
+import Chip from "@mui/material/Chip";
+import axios from "axios";
+import { useEffect } from "react";
+
+const Genres = ({
+  selectedGenres,
+  setSelectedGenres,
+  setGenres,
+  genres,
+  type,
+  setPage,
+}) => {
+  const handleAdd = (genre) => {
+    setSelectedGenres([...selectedGenres, genre]);
+    setGenres(genres.filter((g) => g.id !== genre.id));
+    setPage(1);
+  };
+
+  const handleRemove = (genre) => {
+    setSelectedGenres(
+      selectedGenres.filter((selected) => selected.id !== genre.id)
+    );
+    setGenres([...genres, genre]);
+    setPage(1);
+  };
+
+  const fetchGenres = async () => {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/genre/${type}/list?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
+    );
+    setGenres(data.genres);
+  };
+
+  useEffect(() => {
+    fetchGenres();
+
+    return () => {
+      setGenres({}); // unmounting
+    };
+    // eslint-disable-next-line
+  }, []);
+  // check if genres is Array and its empty or not
+  // {
+  //   Array.isArray(genres) &&
+  //     genres.length > 0 &&
+  //     console.log(`the value of genres is ${genres}`);
+  // }
+  return (
+    <div style={{ padding: "6px 0" }}>
+      {selectedGenres.map((genre) => (
+        <Chip
+          style={{ margin: 2 }}
+          label={genre.name}
+          key={genre.id}
+          color="primary"
+          clickable
+          size="small"
+          onDelete={() => handleRemove(genre)}
+        />
+      ))}
+      {Array.isArray(genres) &&
+        genres.length > 0 &&
+        genres.map((genre) => (
+          <Chip
+            style={{ margin: 2, color: "white" }}
+            label={genre.name}
+            key={genre.id}
+            clickable
+            size="small"
+            onClick={() => handleAdd(genre)}
+          />
+        ))}
+    </div>
+  );
+};
+
+export default Genres;
