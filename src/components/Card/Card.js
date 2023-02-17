4d799da60b8bf9962a9c4e47aaab0ae8e3dbdc0c
code-today <blogarpit21@gmail.com>
2023-02-17 22:59:06 +0530

    created content Modal component for cards

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,25 +2,28 @@ import React from "react";
 import "./Card.css";
 import { img_300, unavailable } from "../../config/config";
 import Badge from "../Badge/Badge";
+import ContentModal from "../ContentModal/ContentModal";
 const Card = ({ id, title, poster, date, media_type, vote_average }) => {
   return (
-    <div className="card">
-      <Badge vote={vote_average} />
-      <div className="posterdiv">
-        <img
-          className="poster"
-          src={poster ? `${img_300}/${poster}` : unavailable}
-          alt={title}
-        />
-      </div>
-      <div className="title">{title}</div>
-      <span className="subtitle">
-        <div className="media_type">
-          {media_type === "tv" ? "TV Series" : "Movie"}
+    <ContentModal media_type={media_type} id={id}>
+      <div className="card">
+        <Badge vote={vote_average} />
+        <div className="posterdiv">
+          <img
+            className="poster"
+            src={poster ? `${img_300}/${poster}` : unavailable}
+            alt={title}
+          />
         </div>
-        <div className="date">{date}</div>
-      </span>
-    </div>
+        <div className="title">{title}</div>
+        <span className="subtitle">
+          <div className="media_type">
+            {media_type === "tv" ? "TV Series" : "Movie"}
+          </div>
+          <div className="date">{date}</div>
+        </span>
+      </div>
+    </ContentModal>
   );
 };
 
---
+++ src/components/ContentModal/ContentModal.js
@@ -0,0 +1,118 @@
+import React, { useEffect, useState } from "react";
+import Modal from "@mui/material/Modal";
+import Backdrop from "@mui/material/Backdrop";
+import Fade from "@mui/material/Fade";
+
+import axios from "axios";
+import {
+  img_500,
+  unavailable,
+  unavailableLandscape,
+} from "../../config/config";
+import "./ContentModal.css";
+
+export default function TransitionsModal({ children, media_type, id }) {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const [content, setContent] = useState();
+  const [video, setVideo] = useState();
+
+  const fetchData = async () => {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/${media_type}/${id}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
+    );
+
+    setContent(data);
+    // console.log(data);
+  };
+
+  const fetchVideo = async () => {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/${media_type}/${id}/videos?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
+    );
+
+    setVideo(data.results[0]?.key);
+  };
+
+  useEffect(() => {
+    fetchData();
+    fetchVideo();
+    // eslint-disable-next-line
+  }, []);
+
+  return (
+    <>
+      <div className="media" color="inherit" onClick={handleOpen}>
+        {children}
+      </div>
+
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className="modal"
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          {content && (
+            <div className="paper">
+              <div className="ContentModal">
+                <img
+                  src={
+                    content.poster_path
+                      ? `${img_500}/${content.poster_path}`
+                      : unavailable
+                  }
+                  alt={content.name || content.title}
+                  className="ContentModal__portrait"
+                />
+                <img
+                  src={
+                    content.backdrop_path
+                      ? `${img_500}/${content.backdrop_path}`
+                      : unavailableLandscape
+                  }
+                  alt={content.name || content.title}
+                  className="ContentModal__landscape"
+                />
+                <div className="ContentModal__about">
+                  <span className="ContentModal__title">
+                    {content.name || content.title} (
+                    {(
+                      content.first_air_date ||
+                      content.release_date ||
+                      "-----"
+                    ).substring(0, 4)}
+                    )
+                  </span>
+                  {content.tagline && (
+                    <i className="tagline">{content.tagline}</i>
+                  )}
+                  <span className="ContentModal__description">
+                    {content.overview}
+                  </span>
+
+                  {/* youtube button */}
+                  <a
+                    target="__blank"
+                    href={`https://www.youtube.com/watch?v=${video}`}
+                  >
+                    <button utton className="youtubeBtn">
+                      Watch the Trailer
+                    </button>
+                  </a>
+                </div>
+              </div>
+            </div>
+          )}
+        </Fade>
+      </Modal>
+    </>
+  );
+}
